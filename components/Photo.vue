2792ad34691a3493c91185458a3fd6190680c684
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2021-12-10 17:50:10 +0800

    aos added

--- components/Photo.vue
+++ components/Photo.vue
@@ -1,5 +1,7 @@
 <template>
-  <img src="~/assets/img/photo.png" alt="photo profile" />
+  <div class="green-circle">
+    <img src="~/assets/img/photo.png" alt="photo profile" />
+  </div>
 </template>
 
 <script>
@@ -7,23 +9,45 @@ export default {}
 </script>
 
 <style scoped>
+.green-circle {
+  position: relative;
+  background: #00dc82;
+  height: 15rem;
+  width: 15rem;
+  border-radius: 50%;
+}
+
 img {
   position: absolute;
-  width: 60%;
-  margin-left: auto;
-  margin-right: auto;
+  top: 1rem;
   left: 0;
   right: 0;
-  bottom: 0;
-  z-index: 1;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
 }
 
+/* md */
 @media (min-width: 768px) {
+  .green-circle {
+    height: 20rem;
+    width: 20rem;
+  }
+
+  img {
+    top: 2rem;
+  }
 }
 
+/* lg */
 @media (min-width: 1024px) {
 }
 
+/* xl */
 @media (min-width: 1280px) {
+  .green-circle {
+    height: 25rem;
+    width: 25rem;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="text-white">
-    <section class="py-20 lg:py-48 relative">
+    <section class="py-20 lg:py-32 relative">
       <div
-        class="container mx-auto px-4 flex flex-col md:flex-row gap-10 items-center"
+        class="container mx-auto px-4 flex flex-col lg:flex-row gap-10 items-center text-center lg:text-left"
       >
-        <div class="flex-1">
-          <div class="text-5xl font-bold mb-6">Hi 👋</div>
-          <div class="text-lg mb-6">
+        <div class="flex-1" data-aos="fade-up">
+          <div class="text-5xl font-bold">Hi 👋</div>
+          <div class="text-lg mt-6">
             You can call me <b>Abed</b>. I am a Frontend Web Developer based in
             Makassar, Indonesia. For the past two years, I have professional
             experience in the IT industry, mainly website development.
@@ -15,27 +15,32 @@
           </div>
 
           <button
-            class="bg-ijo px-3 py-2 rounded text-sm hover:bg-ijoterang text-black"
+            class="bg-ijo px-3 py-2 rounded text-sm hover:bg-ijoterang text-black mt-6"
           >
             VIEW MY RESUME
           </button>
         </div>
 
-        <div class="flex-1">
+        <div class="flex-1 flex justify-center items-center" data-aos="fade-up">
           <Photo />
-          <Background />
         </div>
 
-        <div class="flex-1">test</div>
+        <div class="flex-1" data-aos="fade-up">
+          <div class="text-lg">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta
+            voluptatibus enim assumenda, delectus repellendus perspiciatis amet
+            ut nisi officiis, adipisci tempore modi vitae earum dolorum ratione
+            temporibus. Repellat, dolores sequi.
+          </div>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import Background from "~/components/Background.vue"
 import Photo from "~/components/Photo.vue"
 export default {
-  components: { Background, Photo },
+  components: { Photo },
 }
 </script>
