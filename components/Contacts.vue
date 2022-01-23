427e069076fdeb6caded3129d3751a6d94e86374
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2022-01-24 01:44:17 +0800

    contacts changes

--- components/Contacts.vue
+++ components/Contacts.vue
@@ -2,9 +2,7 @@
   <div>
     <div class="text-center font-montserrat font-bold text-3xl">Contacts</div>
 
-    <div
-      class="mt-10 flex flex-col md:flex-row items-center gap-8 justify-center"
-    >
+    <div class="mt-10 flex items-center gap-8 justify-center">
       <div v-for="contact in contacts" :key="contact.name">
         <a
           :href="contact.link"
@@ -14,11 +12,8 @@
         >
           <div
             v-html="require(`~/assets/icons/${contact.name}.svg?raw`)"
-            class="h-8 w-8"
+            class="h-10 w-10"
           />
-          <div class="ml-2 font-medium">
-            {{ contact.text }}
-          </div>
         </a>
       </div>
     </div>
@@ -32,17 +27,14 @@ export default {
       contacts: [
         {
           name: "email",
-          text: "EMAIL",
           link: "mailto:kalamangna@gmail.com",
         },
         {
           name: "github",
-          text: "GITHUB",
           link: "https://github.com/kalamangna",
         },
         {
           name: "linkedin",
-          text: "LINKEDIN",
           link: "https://www.linkedin.com/in/kalamangna",
         },
       ],
--- components/Stacks.vue
+++ components/Stacks.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="text-center font-montserrat font-bold text-3xl">Stacks</div>
 
-    <div class="mt-12 flex justify-center">
+    <div class="mt-10 flex justify-center">
       <div v-for="{ name } in stacks" :key="name">
         {{ name }}
       </div>
